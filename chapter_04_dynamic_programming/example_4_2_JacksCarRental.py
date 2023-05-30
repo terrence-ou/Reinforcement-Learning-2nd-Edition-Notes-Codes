1f890148b9e525f2d389d90cd91308cc5213bbea
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-05-30 13:45:15 -0400

    example 4.2 updated

--- chapter_04_dynamic_programming/example_4_2_JacksCarRental.py
+++ chapter_04_dynamic_programming/example_4_2_JacksCarRental.py
@@ -9,13 +9,29 @@ MAX_CAPACITY = 20
 TRANSFER_MAX = 5
 GAMMA = 0.9
 
+# Plot values in 3D space
+def plot_3d_value(values):
+    fig = plt.figure(figsize=plt.figaspect(0.5))
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
+    values = values.reshape(21, 21)
+    X = np.arange(0, MAX_CAPACITY + 1)
+    Y = np.arange(0, MAX_CAPACITY + 1)
+    X, Y = np.meshgrid(X, Y)
+    ax.plot_wireframe(X, Y, values, rstride=1, cstride=1)
+    plt.show()
 
-def get_transition_probs(P, s, policy):
-    probs = P[:, s]
-    return probs[range((MAX_CAPACITY + 1) ** 2), policy]
 
-def get_rewards(R, s, policy):
-    return R[s, policy[s] + TRANSFER_MAX]
+def plot_policy(policy, iter=0):
+    ax = sns.heatmap(policy.reshape(21, 21),
+                     cmap='mako')
+    ax.invert_yaxis()
+    ax.set_xticks([0, 21], [0, 20])
+    ax.set_yticks([0, 21], [0, 20])
+    ax.set_xlabel('# Cars at second location')
+    ax.set_ylabel('# Cars at first location')
+    ax.set_title(f'$\pi_{iter}$', fontsize=16)
+    plt.show()
+
 
 
 if __name__ == "__main__":
@@ -27,31 +43,42 @@ if __name__ == "__main__":
 
     # 1. Initialization
     policy = np.zeros(shape=((MAX_CAPACITY+1)**2), dtype=np.int32)
-    values = np.zeros(shape=((MAX_CAPACITY+1)**2), dtype=np.float64)
+    values = np.zeros(shape=((MAX_CAPACITY+1)**2), dtype=np.float32)
     converged = False
 
 
     while not converged:
+        test_round = 0
         # 2. Policy evaluation
         while True:
             delta = 0
             old_values = np.copy(values)
             for s in range(env.nS):
-                probs = get_transition_probs(P, s, policy) # shape: (441, )
-                reward = get_rewards(R, s, policy)
-                # new_val = rewards + GAMMA * probs * values
+                probs = P[:, s, policy[s] + TRANSFER_MAX]
+                reward = R[s, policy[s] + TRANSFER_MAX]
                 new_val = probs * (reward + GAMMA * values)
                 values[s] = new_val.sum()
             diff = abs(old_values - values).max()
-            # print(diff)
             delta = max(delta, diff)
-            if delta < 1e-6: 
+            if delta < 1e-10: 
                 break
         print(values.mean())
 
-        # 3. Policy improvement
+        #3. Policy improvement
         policy_stable = True
+        old_policy = np.copy(policy)
+        
         for s in range(env.nS):
-            break
+            probs = P[:, s].T
+            reward = R[s]
+            pi_candidates = reward + GAMMA * probs @ values
+            policy[s] = pi_candidates.argmax() - TRANSFER_MAX
+            
+            if policy[s] != old_policy[s]:
+                policy_stable = False
 
-        break
+        if policy_stable: break
+
+
+    # plot_policy(policy, 4)
+    plot_3d_value(values)
