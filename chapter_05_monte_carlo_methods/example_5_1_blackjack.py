37f4a15101ae96233a223bf96d06d11e37fec98b
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-06-18 01:34:10 -0400

    chapter 5 updated

--- chapter_05_monte_carlo_methods/example_5_1_blackjack.py
+++ chapter_05_monte_carlo_methods/example_5_1_blackjack.py
@@ -1,7 +1,32 @@
+
+##############################################
+# This example is still in progress
+##############################################
+
 import numpy as np
 import gymnasium as gym
+
+# In example 5.1, the policy is:
+# - stick (0) if the player's sum is 20 or 21
+# - hits (1) otherwise
+def policy(curr_sum:int) -> int:
+    return 0 if curr_sum >= 20 else 1
+
 
 if __name__ == "__main__":
     # Create blackjack environment
     # Turn sab True to make the env follows the S&B books reward
     env = gym.make("Blackjack-v1", sab=True)
+    # Observation -> (player_sum, dealer_showing, usable_ace)
+    player_sums = env.observation_space[0].n
+    dealer_faces = env.observation_space[1].n
+    
+    # Initialize V
+    V = np.zeros(shape=(player_sums, dealer_faces))
+    # Initialize Returns
+    returns = []
+    num_episodes = 10_000
+
+
+    
+    observation, info = env.reset()
