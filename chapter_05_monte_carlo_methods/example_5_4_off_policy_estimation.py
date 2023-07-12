82c8c8644ac06b3525753569026d8cba92971d16
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-07-11 21:01:57 -0400

    example 5.4 added

---
+++ chapter_05_monte_carlo_methods/example_5_4_off_policy_estimation.py
@@ -0,0 +1,18 @@
+'''
+In progress
+'''
+
+import numpy as np
+
+def prob_target_policy(player_sum:int) -> float:
+    raise NotImplementedError
+
+def prob_behavior_policy():
+    raise NotImplementedError
+
+def calc_pho():
+    raise NotImplementedError
+
+def main():
+
+    target_val = -0.27726 # Provided in the book
