0f0300a57eb4a0d766be93339b9b3d0b667783e2
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-07-18 16:03:56 -0400

    exercise 5.12 updated

--- chapter_05_monte_carlo_methods/exercise_5_12_racetrack.py
+++ chapter_05_monte_carlo_methods/exercise_5_12_racetrack.py
@@ -1,8 +1,9 @@
 import numpy as np
-from race_track_env.race_track import RaceTrack
-
+import pickle
 import matplotlib.pyplot as plt
+from scipy.ndimage import uniform_filter
 
+from race_track_env.race_track import RaceTrack
 
 def behavior_pi(state:tuple, 
                 nA:int, 
@@ -26,20 +27,65 @@ def behavior_pi(state:tuple,
             return action, epsilon / nA
 
 
+# Plot the result
+def plot_result(value_hist:dict, total_episodes) -> None:
+    
+    line_width = 1.2
+    fontdict = {'fontsize': 12, 'fontweight': 'bold'}
 
-def off_policy_monte_carlo(track_map:str, render_mode:str):
+    plt.figure(figsize=(10, 6), dpi=150)
+    plt.ylim((-500.0, 0.0))
+    plt.grid(c='lightgray')
+    plt.margins(0.02)
+
+    for i, spine in enumerate(plt.gca().spines.values()):
+        if i in [0, 2]:
+            spine.set_linewidth(1.5)
+            continue
+        spine.set_visible(False)
+    
+    x = np.arange(total_episodes)
+    plt.xscale('log')
+    plt.xticks([1, 10, 100, 1000, 10_000, 100_000, 1_000_000], 
+               ['1', '10', '100', '1000', '10,000', '100,000', '1,000,000'])
+    # plt.yticks()
+    colors = ['tomato', 'cornflowerblue']
+    for i, (key, value) in enumerate(value_hist.items()):
+        title, label = key.split(',')
+        plt.plot(x, uniform_filter(value, size=20), 
+                 linewidth=line_width, 
+                 label=label,
+                 c=colors[i],
+                 alpha=0.95)
+
+    plt.title(title + ' training record', fontdict=fontdict)
+    plt.xlabel('Episodes (log scale)', fontdict=fontdict)
+    plt.ylabel('Rewards', fontdict=fontdict)    
+    plt.legend()
+    # plt.tight_layout()
+    plt.savefig(f'./plots/exercise_5_12/{"_".join(title.lower().split())}.png')
+    plt.show()
+
+
+
+def off_policy_monte_carlo(total_episodes:int,
+                            track_map:str, render_mode:str,
+                            zero_acc:bool=False):
 
     gamma = 0.9
     epsilon = 0.1
-    total_episodes = 100_000
+    # total_episodes = 100_000
 
     env = RaceTrack(track_map, render_mode, size=20)
     action_space = env.nA # (9, ), nine actions in total
     observation_space = env.nS # (curr_row, curr_col, row_speed, col_speed)
 
-    Q = np.random.normal(size=(*observation_space, action_space)) - 500
+    Q = np.random.normal(size=(*observation_space, action_space))
+    Q -= 500 # optimistic initial values
     C = np.zeros_like(Q)
     target_pi = np.argmax(Q, axis=-1)
+
+    reward_hist = np.zeros(shape=(total_episodes), dtype=np.float32)
 
     for i in range(total_episodes):
         
@@ -52,13 +98,18 @@ def off_policy_monte_carlo(track_map:str, render_mode:str):
         ttl_reward = 0
 
         while not terminated:
-            observation, reward, terminated = env.step(action)
+            if zero_acc and np.random.rand() <= 0.1:
+                non_acc_act = 4 # check env._action_to_acceleration
+                observation, reward, terminated = env.step(non_acc_act)
+            else:
+                observation, reward, terminated = env.step(action)
+            
             ttl_reward += reward
             trajectory.append((state, action, reward, act_prob))
             state = observation
             (action, act_prob) = behavior_pi(state, env.nA, target_pi, epsilon)
         
-        G = 0
+        G = 0.
         W = 1.
 
         while trajectory:
@@ -72,11 +123,31 @@ def off_policy_monte_carlo(track_map:str, render_mode:str):
                 break
             W = W * (1 / act_prob)
         
+        reward_hist[i] = ttl_reward
         if i % 1000 == 0:
             print(f'Episode: {i}, reward: {ttl_reward}, epsilon: {epsilon}')
+        
+    return reward_hist, Q
 
 
 if __name__ == "__main__":
 
+    total_episodes = 1_000_000
+    track_map = 'b'
 
-    off_policy_monte_carlo('a', None)
+    reward_hist_dict = dict()
+    Q_dict = dict()
+
+    for i in range(2):
+        track_name = f'Track {track_map.capitalize()}'
+        use_zero_acc = 'with zero acc.' if i else 'without zero acc.'
+        key = track_name + ',' + use_zero_acc
+
+        reward_hist, Q = off_policy_monte_carlo(total_episodes, track_map, None, i)
+        reward_hist_dict[key] = reward_hist
+        Q_dict[key] = Q
+    
+    plot_result(reward_hist_dict, total_episodes)
+    with open(f'./history/exercise_5_12/track_{track_map}.pkl', 'wb') as f:
+        pickle.dump(Q_dict)
+
