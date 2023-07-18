16ceccd860fc298183fa067b31654e078bc567e8
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-07-17 22:30:00 -0400

    race_track env updated

--- chapter_05_monte_carlo_methods/race_track_env/race_track.py
+++ chapter_05_monte_carlo_methods/race_track_env/race_track.py
@@ -25,6 +25,7 @@ class RaceTrack(Env):
         with open('./race_track_env/maps/' + filename, 'rb') as f:
             self.track_map = np.load(f)
 
+        # Initialize parameters for pygame
         self.window_size = self.track_map.shape
         # Pygame's coordinate if the transpose of that of numpy
         self.window_size = (self.window_size[1] * self.size, self.window_size[0] * self.size)
@@ -66,6 +67,7 @@ class RaceTrack(Env):
     # Get observation
     def _get_obs(self):
         return (*self.state, *self.speed)
+
     # Get info, always return None in our case
     def _get_info(self):
         return None
@@ -87,7 +89,6 @@ class RaceTrack(Env):
         if row < 0 or row >= H or col < 0 or col >= W:
             return True
         # Check if the car run into the gravels
-        # print(next_state)
         if self.track_map[next_state[0], next_state[1]] == 0:
             return True
         return False
@@ -95,12 +96,15 @@ class RaceTrack(Env):
 
     # reset the car to one of the starting positions
     def reset(self):
+        # Select start position randomly from the starting line
         start_idx = np.random.choice(self.start_states.shape[0])
         self.state = self.start_states[start_idx]
         self.speed = (0, 0)
+
         if self.render_mode == 'human':
             self.render(self.render_mode)
         return self._get_obs(), self._get_info()
+
 
     # take actions
     def step(self, action):
@@ -112,38 +116,39 @@ class RaceTrack(Env):
         temp_x_acc = self.speed[1] + x_act
         
         if temp_y_acc < -4: temp_y_acc = -4
-        # if temp_y_acc > 4: temp_y_acc = 4
-        if temp_y_acc > 0: temp_y_acc = 0
+        if temp_y_acc > 0: temp_y_acc = 0 # Avoid the car from going backward
         if temp_x_acc < -4: temp_x_acc = -4
-        # if temp_x_acc < 0: temp_x_acc = 0
         if temp_x_acc > 4: temp_x_acc = 4
         
         new_state[0] += temp_y_acc
         new_state[1] += temp_x_acc
         
         terminated = False
-        # check next position
+        # check if next position crosses the finishing line
         if self._check_finish():
             terminated = True 
+        # check if next postion locates in invalid places
         elif self._check_out_track(new_state):
             self.reset()
         else:
             self.state = new_state
             self.speed = (temp_y_acc, temp_x_acc)
+
         if self.render_mode == 'human':
             self.render(self.render_mode)
         
         return self._get_obs(), -1, terminated
 
 
-
     # visualize race map
     def render(self, mode):
+
         if self.window is None:
             pygame.init()
             pygame.display.set_caption('Race Track')
             if mode == 'human':
                 self.window = pygame.display.set_mode(self.window_size)
+
         if self.clock is None:
             self.clock = pygame.time.Clock()
 
@@ -154,22 +159,27 @@ class RaceTrack(Env):
         for row in range(rows):
             for col in range(cols):
                 cell_val = self.track_map[row, col]
-                if cell_val == 0.4:
-                    fill = (255, 0, 0)
+                # Draw finishing cells
+                if cell_val == FINISHING:
+                    fill = (235, 52, 52)
                     pygame.draw.rect(self.window, fill, (col * self.size, row * self.size, self.size, self.size), 0)
-                elif cell_val == 0.8:
-                    fill = (0, 255, 0)            
+                # Draw starting cells
+                elif cell_val == STARTING:
+                    fill = (61, 227, 144)            
                     pygame.draw.rect(self.window, fill, (col * self.size, row * self.size, self.size, self.size), 0)
-        
-                color = (0, 0, 0)
+
+                color =(120, 120, 120)
+                # Draw gravels
                 if cell_val == 0:
                     color = (255, 255, 255)
+                # Draw race track
                 elif cell_val == 1:
-                    color = (100, 100, 100)
+                    color = (160, 160, 160)
+                
                 pygame.draw.rect(self.window, color, (col * self.size, row * self.size, self.size, self.size), 1)
         
         # Draw the car
-        pygame.draw.rect(self.window, (0, 0, 255), (self.state[1] * self.size, self.state[0] * self.size, self.size, self.size), 0)
+        pygame.draw.rect(self.window, (86, 61, 227), (self.state[1] * self.size, self.state[0] * self.size, self.size, self.size), 0)
 
 
         # if mode == "human":
@@ -185,7 +195,9 @@ class RaceTrack(Env):
 
 if __name__ == '__main__':
 
-    env = RaceTrack('a', render_mode=None, size=20)
+    # render_mode = 'human'
+    render_mode = None
+    env = RaceTrack('a', render_mode=render_mode, size=20)
     env.reset()
     total_reward = 0
     terminated = False
