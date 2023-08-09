3d2ae325aab4b1397d1195f7e8a64993e7f69810
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-08-08 21:44:59 -0400

    example 6.2 updated

--- chapter_06_temporal_difference_learning/example_6_2_random_walk.py
+++ chapter_06_temporal_difference_learning/example_6_2_random_walk.py
@@ -1,47 +1,59 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
+# Using node to represent the states and the connection between each pair
 class Node:
     def __init__(self, val: str):
         self.value = val
         self.right = None
         self.left = None
+        self.r_reward = 0  # the reward of stepping right
+        self.l_reward = 0  # the reward of stepping right
 
     def __eq__(self, other_val) -> bool:
         return self.value == other_val
+    
+    def __repr__(self) -> str:
+        return f"Node {self.value}"
 
 
 # Build the Random Walk environment
 class RandomWalk:
     def __init__(self):
+        self.states = ["A", "B", "C", "D", "E"]
+        self.state_idx_map = {letter: idx for idx, letter in enumerate(self.states)}
         self.nodes = self.build_env()
 
-    def step(self):
+    def step(self, action: int) -> tuple:
         raise NotImplementedError
 
     # building the random walk environment as a linked list
-    def build_env(self):
-        values = ["A", "B", "C", "D", "E"]
+    def build_env(self) -> Node:
+        values = self.states
         head = Node(values[0])
         builder = head
         prev = None
         for i, val in enumerate(values):
-            next = None if i == len(values) - 1 else Node(values[i + 1])
+            next_node = None if i == len(values) - 1 else Node(values[i + 1])
+            if not next_node:
+                builder.r_reward = 1
+
             builder.left = prev
-            builder.right = next
+            builder.right = next_node
             prev = builder
-            builder = next
+            builder = next_node
         return head
 
 
-# TODO: write a test block here
-def test_RandomWalk_nodes():
-    fake_env = RandomWalk()
-    head = fake_env.nodes
+# Check the nodes and rewards
+def check_RandomWalk_nodes():
+    head = RandomWalk().build_env()
     while head:
-        print(head.value)
+        print("Links: \t", head.left, "←", head, "→", head.right)
+        print("Reward: \t", head.l_reward, "←", head, "→", head.r_reward, "\n")
         head = head.right
 
 
 if __name__ == "__main__":
-    test_RandomWalk_nodes()
+    check_RandomWalk_nodes()
