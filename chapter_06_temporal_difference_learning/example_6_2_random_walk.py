51c1c8eba96eda54cad32d68c0c09ae73f41f488
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-08-09 19:02:28 -0400

    example 6.2 updated

--- chapter_06_temporal_difference_learning/example_6_2_random_walk.py
+++ chapter_06_temporal_difference_learning/example_6_2_random_walk.py
@@ -18,8 +18,8 @@ def random_policy() -> int:
 # TD algorithm
 def TD_evaluation(alpha: float = 0.1, num_episodes=100) -> np.ndarray:
     env = RandomWalk()
-    total_states = len(env.state_space) + 2  # also include terminal states in both end
-    # Initialize value
+    total_states = len(env.state_space) + 2  # also include terminal states on both ends
+    # Initialize values
     V = np.zeros(shape=(total_states), dtype=float)
     V[1:-1] += 0.5  # initial values of non-terminal state to 0.5
     gamma = 1.0
@@ -42,7 +42,34 @@ def TD_evaluation(alpha: float = 0.1, num_episodes=100) -> np.ndarray:
 
 # MC algorithm
 def MC_evaluation(alpha: float = 0.1, num_episodes=100) -> np.ndarray:
-    raise NotImplementedError
+    env = RandomWalk()
+    total_states = len(env.state_space) + 2  # include terminal states on both ends
+    # Initialize values
+    V = np.zeros(shape=(total_states), dtype=float)
+    V[1:-1] += 0.5
+    gamma = 1.0
+
+    V_history = np.zeros(shape=(num_episodes, total_states))
+
+    # Run MC algorithm
+    for episode in range(num_episodes):
+        env.reset()
+        state = env.initial_idx
+        terminated = False
+        traj = []  # A list to store trajectories of the episode
+        while not terminated:
+            action = random_policy()
+            next_state, reward, terminated = env.step(action)
+            traj.append((state, action, reward))
+            state = next_state
+
+        G = 0  # The returns of the trajectory
+        while traj:
+            (state, action, reward) = traj.pop()
+            G = gamma * G + reward
+            V[state] = V[state] + alpha * (G - V[state])
+        V_history[episode] = np.copy(V)
+    return V_history
 
 
 ########################################################################
@@ -53,12 +80,37 @@ def MC_evaluation(alpha: float = 0.1, num_episodes=100) -> np.ndarray:
 # For the value approximation example
 def estimate_value(num_episodes: int, true_value: np.ndarray) -> None:
     V_hist_single = TD_evaluation(alpha=0.1, num_episodes=num_episodes)
-    selected_hist = V_hist_single[(0, 9, 99), 1:-1]
+    sel_epochs = (0, 9, 99)
+    selected_hist = V_hist_single[sel_epochs, 1:-1]
 
-    plt.plot(true_value)
+    # Plotting the result
+    font_dict = {"fontsize": 11}
+    x_axis = np.arange(5, dtype=int)
+    colors = ["mediumseagreen", "steelblue", "orchid"]
 
-    for hist in selected_hist:
-        plt.plot(hist)
+    plt.figure(figsize=(6, 6), dpi=150)
+    plt.grid(c="lightgray")
+    plt.margins(0.02)
+    plt.xticks(x_axis, ["A", "B", "C", "D", "E"])
+    for i, spine in enumerate(plt.gca().spines.values()):
+        if i in [0, 2]:
+            spine.set_linewidth(1.5)
+            continue
+        spine.set_visible(False)
+    plt.xlabel("State", fontdict=font_dict)
+    plt.ylabel("Estimated value", fontdict=font_dict)
+
+    plt.scatter(x_axis, true_value, c="black", s=10.0)
+    plt.plot(x_axis, true_value, c="black", linewidth=1.6, label="True values")
+
+    for i, hist in enumerate(selected_hist):
+        plt.scatter(x_axis, hist, s=7.0, c=colors[i])
+        plt.plot(
+            x_axis, hist, linewidth=1.2, c=colors[i], label=f"sweep {sel_epochs[i]+1}"
+        )
+    plt.title("The values learned after $n$ sweeps", fontweight="bold", fontsize=13)
+    plt.legend(loc=4)
+    plt.savefig("./plots/example_6_2/value_approx.png")
     plt.show()
 
 
@@ -71,6 +123,7 @@ def rms(V_hist: np.ndarray, true_value: np.ndarray) -> np.ndarray:
     return rooted_mse
 
 
+# Run TD evaluation for given episodes
 def parameter_sweep_td(
     alpha_list: list, num_episodes: int, true_value: np.ndarray
 ) -> None:
@@ -78,16 +131,26 @@ def parameter_sweep_td(
     V_hist = np.zeros(shape=(num_runs, num_episodes, 5))
     for alpha in alpha_list:
         for i in range(num_runs):
-            v_single = TD_evaluation(alpha=alpha)
+            v_single = TD_evaluation(alpha=alpha, num_episodes=num_episodes)
             V_hist[i] = v_single[:, 1:-1]
 
         error = rms(V_hist, true_value)
         plt.plot(error)
-    plt.show()
+    # plt.show()
 
 
+# Run MC evaluation for given episodes
 def parameter_sweep_mc(alpha_list, num_episodes, true_value) -> None:
-    raise NotImplementedError
+    num_runs = 100
+    V_hist = np.zeros(shape=(num_runs, num_episodes, 5))
+    for alpha in alpha_list:
+        for i in range(num_runs):
+            v_single = MC_evaluation(alpha=alpha, num_episodes=num_episodes)
+            V_hist[i] = v_single[:, 1:-1]
+
+        error = rms(V_hist, true_value)
+        plt.plot(error)
+    # plt.show()
 
 
 if __name__ == "__main__":
@@ -96,11 +159,13 @@ if __name__ == "__main__":
 
     # The true value of the random policy for RW is provided as follows
     true_value = np.arange(0, 1.1, 1 / 6)[1:-1]
-    num_episodes = 100
-    alpha_list = [0.05, 0.1, 0.15]
-
+    num_episodes = 150
     # Example a: value function approximation
-    estimate_value(num_episodes=num_episodes, true_value=true_value)
+    # estimate_value(num_episodes=num_episodes, true_value=true_value)
 
     # Example b: RMS error over different setups
-    # parameter_sweep_td(alpha_list, num_episodes, true_value)
+    alpha_list = [0.05, 0.1, 0.15]
+    parameter_sweep_td(alpha_list, num_episodes, true_value)
+    alpha_list = [0.01, 0.02, 0.03, 0.04]
+    parameter_sweep_mc(alpha_list, num_episodes, true_value)
+    plt.show()
