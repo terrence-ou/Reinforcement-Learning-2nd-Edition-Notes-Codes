0e1856144e5b0bad35a3d1e2709544b53d366743
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-08-10 15:00:28 -0400

    example 6.3 updated

---
+++ chapter_06_temporal_difference_learning/example_6_3_batch_updating.py
@@ -0,0 +1,14 @@
+import numpy as np
+from envs.random_walk_env import RandomWalk
+
+
+def td_updating():
+    raise NotImplementedError
+
+
+def mc_updating():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    print("Initialized!")
