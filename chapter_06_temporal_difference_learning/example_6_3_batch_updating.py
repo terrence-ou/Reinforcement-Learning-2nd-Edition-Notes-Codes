3eedcca33fa519249d853cb8501de87d944dfbdf
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-08-10 23:20:57 -0400

    example 6.3 updated

--- chapter_06_temporal_difference_learning/example_6_3_batch_updating.py
+++ chapter_06_temporal_difference_learning/example_6_3_batch_updating.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from envs.random_walk_env import RandomWalk
 
+THRESHOLD = 1e-3
+
+########################################################################
+###                          Helper Functions                        ###
+########################################################################
+
 
 # Random policy
 def random_policy() -> int:
@@ -36,21 +42,51 @@ def generate_trajectory(env: RandomWalk) -> list:
 ########################################################################
 
 
-# update by visiting all trajectories in the batch
 def td_single_batch(
     V: np.ndarray, alpha: float, trajs: list, gamma: float = 1.0
 ) -> np.ndarray:
-    increments = np.zeros_like(V)
-    for traj in trajs:
-        for state, next_state, reward in traj:
-            increments[state] += alpha * (reward + gamma * V[next_state] - V[state])
-
-    V += increments
+    while True:
+        increments = np.zeros_like(V)
+        for traj in trajs:
+            for state, next_state, reward in traj:
+                increments[state] += alpha * (reward + gamma * V[next_state] - V[state])
+        if np.abs(increments).sum() < THRESHOLD:
+            break
+        V += increments
     return V
 
 
-# TD evaluation algorithm
-def td_evaluation(num_episodes: int, alpha: float):
+########################################################################
+###                     MC Batch Update algorithm                    ###
+########################################################################
+
+
+def mc_single_batch(
+    V: np.ndarray, alpha: float, trajs: list, gamma: float = 1.0
+) -> np.ndarray:
+    while True:
+        increments = np.zeros_like(V)
+        for traj in trajs:
+            G = 0
+            for i in range(len(traj) - 1, -1, -1):
+                state, _, reward = traj[i]
+                G = gamma * G + reward
+                increments[state] += alpha * (G - V[state])
+        if np.abs(increments).sum() < THRESHOLD:
+            break
+        V += increments
+    return V
+
+
+########################################################################
+###                         Wraper functions                         ###
+########################################################################
+
+
+# evaluate the selected algorithm
+def evaluation(num_episodes: int, alpha: float, mode: str) -> np.ndarray:
+    assert mode in ["MC", "TD"], "mode should be either MC or TD"
+
     env = RandomWalk()
     total_states = len(env.state_space) + 2
 
@@ -62,37 +98,31 @@ def td_evaluation(num_episodes: int, alpha: float):
     for episode in range(num_episodes):
         traj = generate_trajectory(env)
         batch.append(traj)
-        V = td_single_batch(V, alpha, batch)
+        if mode == "MC":
+            V = mc_single_batch(V, alpha, batch)
+        else:
+            V = td_single_batch(V, alpha, batch)
         V_history[episode] = np.copy(V)
 
     return V_history
 
 
-########################################################################
-###                     MC Batch Update algorithm                    ###
-########################################################################
-def mc_batch_update():
-    raise NotImplementedError
-
-
-def mc_updating():
-    raise NotImplementedError
-
-
-########################################################################
-###                         Wraper functions                         ###
-########################################################################
+# run the example
+def run_bacth_update(num_episodes: int, num_runs: int, alpha: float) -> None:
+    true_value = np.arange(0, 1.1, 1 / 6)[1:-1]
+    for mode in ["TD", "MC"]:
+        V_hist = np.zeros(shape=(num_runs, num_episodes, 5))
+        for i in range(num_runs):
+            error_hist = evaluation(num_episodes, alpha, mode=mode)
+            V_hist[i] = error_hist[:, 1:-1]
+        avg_error = rms(V_hist, true_value)
+        plt.plot(avg_error, label=mode)
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
     num_episodes = 100
-    num_runs = 100
-    true_value = np.arange(0, 1.1, 1 / 6)[1:-1]
-    alpha = 4e-3
-    V_hist = np.zeros(shape=(num_runs, num_episodes, 5))
-    for i in range(num_runs):
-        td_error_hist = td_evaluation(num_episodes, alpha)
-        V_hist[i] = td_error_hist[:, 1:-1]
-    error = rms(V_hist, true_value)
-    plt.plot(error)
-    plt.show()
+    num_runs = 1
+    alpha = 1e-3
+    run_bacth_update(num_episodes, num_runs, alpha)
