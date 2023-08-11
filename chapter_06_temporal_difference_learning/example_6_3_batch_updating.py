2839659aa679b61aacf0e5c18c4d788e064b9c20
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-08-10 21:10:15 -0400

    example 6.3 updated

--- chapter_06_temporal_difference_learning/example_6_3_batch_updating.py
+++ chapter_06_temporal_difference_learning/example_6_3_batch_updating.py
@@ -2,8 +2,44 @@ import numpy as np
 from envs.random_walk_env import RandomWalk
 
 
-def td_updating():
+def random_policy() -> int:
+    return np.random.choice(2)
+
+
+# Generate a trajectory based on the environment and random policy
+def generate_trajectory(env: RandomWalk) -> list:
+    trajectory = []
+    env.reset()
+    state = env.initial_idx
+    terminated = False
+    while not terminated:
+        action = random_policy()
+        next_state, reward, terminated = env.step(action)
+        trajectory.append((state, next_state, reward))
+        state = next_state
+    return trajectory
+
+
+def td_batch_update(
+    V: np.ndarray, alpha: float, trajs: list, gamma: float = 1.0
+) -> np.ndarray:
+    increments = np.zeros_like(V)
+    for traj in trajs:
+        for state, next_state, reward in traj:
+            increments[state] = alpha * (reward + gamma * V[next_state] - V[state])
+    V += increments
+    return V
+
+
+def mc_batch_update():
     raise NotImplementedError
+
+
+def td_evaluation(num_episodes: int):
+    env = RandomWalk()
+    total_states = len(env.state_space) + 2
+
+    V = np.zeros(shape=(total_states), dtype=float)
 
 
 def mc_updating():
@@ -11,4 +47,6 @@ def mc_updating():
 
 
 if __name__ == "__main__":
-    print("Initialized!")
+    env = RandomWalk()
+    test_traj = generate_trajectory(env)
+    print(test_traj)
