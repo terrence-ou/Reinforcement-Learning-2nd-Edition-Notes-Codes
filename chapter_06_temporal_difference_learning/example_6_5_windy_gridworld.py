2d7afd9dde3eb55ee65b326238e06556bbde9873
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-08-14 14:50:51 -0400

    example 6.5 updated

--- chapter_06_temporal_difference_learning/example_6_5_windy_gridworld.py
+++ chapter_06_temporal_difference_learning/example_6_5_windy_gridworld.py
@@ -11,11 +11,14 @@ def epsilon_greedy(Q: np.ndarray, state: tuple, epsilon: float = 0.1) -> int:
     return Q[state].argmax()
 
 
-def SARSA(env: Any, alpha: float, epsilon: float, total_steps: int = 8000):
+# SARSA Algorithm
+def SARSA(env: Any, alpha: float, epsilon: float, total_steps: int = 8000) -> tuple:
     optim_init = 0
     gamma = 1.0
 
-    Q = np.random.normal(loc=optim_init, scale=1.0, size=(*env.nS, env.nA))
+    # Initialize Q values
+    # Q = np.random.normal(loc=optim_init, scale=1.0, size=(*env.nS, env.nA))
+    Q = np.zeros(shape=(*env.nS, env.nA))
     Q[env.goal] = 0  # Set the value of the terminal state to 0
 
     curr_step = 0
@@ -23,6 +26,7 @@ def SARSA(env: Any, alpha: float, epsilon: float, total_steps: int = 8000):
     step_episode_hist = np.zeros(shape=(total_steps + 1,))
     reward_hist = []
 
+    # Run SARSA for given steps
     while curr_step < total_steps:
         episode_reward = 0
         state = env.reset()
@@ -38,31 +42,43 @@ def SARSA(env: Any, alpha: float, epsilon: float, total_steps: int = 8000):
             action = next_action
             episode_reward += reward
             step_episode_hist[curr_step] = total_episodes
+            # stop conditions
             if terminated or truncated:
                 total_episodes += 1
+                reward_hist.append(episode_reward)
                 break
             if curr_step >= total_steps:
                 break
-    plt.plot(step_episode_hist)
-    plt.show()
+
+    return Q, step_episode_hist, reward_hist
 
 
-def run_sarsa_windy():
-    raise NotImplementedError
+def run_sarsa_windy(
+    epsilon: float, alpha: float, plot: bool = True, render_result: bool = False
+) -> None:
+    render_mode = None
+    env = WindyGridworld(render_mode=render_mode)
+    Q, step_episode_hist, reward_hist = SARSA(env, alpha=alpha, epsilon=epsilon)
+    if plot:
+        plt.plot(step_episode_hist)
+        plt.show()
+        plt.ylim((-300, 0))
+        plt.plot(reward_hist)
+        plt.show()
+
+    if render_result:
+        A = Q.argmax(axis=-1)
+        env = WindyGridworld(render_mode="human")
+        state = env.reset()
+        while True:
+            action = A[state]
+            next_state, reward, terminated, truncated = env.step(action)
+            state = next_state
+            if terminated or truncated:
+                break
 
 
 if __name__ == "__main__":
-    render_mode = None
     epsilon = 0.1
     alpha = 0.5
-    env = WindyGridworld(render_mode=render_mode)
-    # env.reset()
-    # ttl_reward = 0
-    # while True:
-    #     next_state, reward, terminated, truncated = env.step(np.random.choice(4))
-    #     ttl_reward += reward
-    #     if terminated or truncated:
-    #         break
-    # print(ttl_reward)
-
-    SARSA(env=env, alpha=alpha, epsilon=epsilon)
+    run_sarsa_windy(epsilon=epsilon, alpha=alpha, plot=False, render_result=True)
