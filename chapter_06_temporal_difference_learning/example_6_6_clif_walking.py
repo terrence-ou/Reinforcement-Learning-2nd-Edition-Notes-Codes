b5223fadedc8da75b73dbaccdae920f4cc7ec052
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-08-16 01:05:17 -0400

    example 6.6 updated

--- chapter_06_temporal_difference_learning/example_6_6_clif_walking.py
+++ chapter_06_temporal_difference_learning/example_6_6_clif_walking.py
@@ -6,15 +6,18 @@ import matplotlib.pyplot as plt
 import gymnasium as gym
 
 
+# Epsilon-greedy policy
 def epsilon_greedy(Q: np.ndarray, state: int, epsilon: float):
     if np.random.random() < epsilon:
         return np.random.choice(Q.shape[-1])
     return np.argmax(Q[state], axis=-1)
 
 
+# SARSA algorithm
 def SARSA(
     env: Any, epsilon: float, alpha: float, gamma: float, num_episodes: int = 500
 ) -> tuple:
+    # initialize action-value function(Q table)
     nS = env.observation_space.n
     nA = env.action_space.n
     Q = np.zeros(shape=(nS, nA))
@@ -25,14 +28,13 @@ def SARSA(
         state, _ = env.reset()
         episode_reward = 0
         action = epsilon_greedy(Q, state, epsilon)
+        # Single-episode pass
         while True:
             next_state, reward, terminated, truncated, _ = env.step(action)
             next_action = epsilon_greedy(Q, next_state, epsilon)
-
             Q[state, action] = Q[state, action] + alpha * (
                 reward + gamma * Q[next_state, next_action] - Q[state, action]
             )
-
             state = next_state
             action = next_action
             episode_reward += reward
@@ -43,9 +45,11 @@ def SARSA(
     return Q, reward_hist
 
 
+# Q-learning algorithm
 def Q_learning(
     env: Any, epsilon: float, alpha: float, gamma: float, num_episodes: int = 500
 ) -> tuple:
+    # Initialize action-value function (Q table)
     nS = env.observation_space.n
     nA = env.action_space.n
     Q = np.zeros(shape=(nS, nA))
@@ -55,6 +59,7 @@ def Q_learning(
     for i in range(num_episodes):
         state, _ = env.reset()
         episode_reward = 0
+        # Single-episode pass
         while True:
             action = epsilon_greedy(Q, state, epsilon)
             next_state, reward, terminated, truncated, _ = env.step(action)
@@ -69,33 +74,88 @@ def Q_learning(
     return Q, reward_hist
 
 
-if __name__ == "__main__":
-    epsilon = 0.1
-    alpha = 0.5
-    gamma = 1.0
-
-    total_runs = 10
-    total_episodes = 500
-
-    env = gym.make("CliffWalking-v0")
-
+# The wrapper function for
+def run_q_sarsa_comparison(
+    env: Any,
+    total_runs: int,
+    num_episodes: int,
+    epsilon: float,
+    alpha: float,
+    gamma: float,
+) -> tuple:
     reward_hist_Q = np.zeros(shape=(total_runs, 500))
     reward_hist_SAR = np.zeros(shape=(total_runs, 500))
 
     for i in range(total_runs):
-        Q_Q, reward_hist_Q[i] = Q_learning(env, epsilon, alpha, gamma)
-        Q_SARSA, reward_hist_SAR[i] = SARSA(env, epsilon, alpha, gamma)
+        Q_Q, reward_hist_Q[i] = Q_learning(env, epsilon, alpha, gamma, num_episodes)
+        Q_SARSA, reward_hist_SAR[i] = SARSA(env, epsilon, alpha, gamma, num_episodes)
 
-    plt.plot(uniform_filter(reward_hist_Q.mean(axis=0), size=7), label="Q-learning")
-    plt.plot(uniform_filter(reward_hist_SAR.mean(axis=0), size=7), label="SARSA")
-    plt.ylim(-100, 0)
-    plt.legend()
+    return (reward_hist_Q, reward_hist_SAR), (Q_Q, Q_SARSA)
+
+
+# plot reward history
+def plot_history(q_history, sarsa_history):
+    # plot visualization setup
+    font_dict = {"fontsize": 11}
+    plt.figure(figsize=(9, 6), dpi=150)
+    plt.grid(c="lightgray")
+    plt.margins(0.02)
+    for i, spine in enumerate(plt.gca().spines.values()):
+        if i in [0, 2]:
+            spine.set_linewidth(1.5)
+            continue
+        spine.set_visible(False)
+    plt.xlabel("Episodes", fontdict=font_dict)
+    plt.ylabel("Sum of rewards during each episode", fontdict=font_dict)
+    plt.title(
+        "Comparison between Q-learning and SARSA on Cliff Walking",
+        fontsize=13,
+        fontweight="bold",
+    )
+    # plot result
+    q_history = uniform_filter(q_history.mean(axis=0), size=7)
+    sarsa_history = uniform_filter(sarsa_history.mean(axis=0), size=7)
+    plt.plot(sarsa_history, c="steelblue", label="SARSA")
+    plt.plot(q_history, c="tomato", label="Q-learning")
+    plt.ylim(-100, -15)
+    plt.legend(loc=4)
+    plt.savefig("./plots/example_6_6/rewards.png")
     plt.show()
 
+
+# render out gameplay with given Q value
+def render_result(Q):
     env = gym.make("CliffWalking-v0", render_mode="human")
-    state, _ = env.reset()
-    while True:
-        action = Q_SARSA[state].argmax()
-        state, reward, terminated, truncated, info = env.step(action)
-        if terminated:
-            break
+    for _ in range(10):
+        state, _ = env.reset()
+        while True:
+            action = Q[state].argmax()
+            state, reward, terminated, truncated, info = env.step(action)
+            if terminated:
+                break
+
+
+if __name__ == "__main__":
+    # Hyperparameters
+    epsilon = 0.1
+    alpha = 0.5
+    gamma = 1.0
+
+    total_runs = 80
+    num_episodes = 500
+
+    # Create environment
+    env = gym.make("CliffWalking-v0")
+
+    # Run both algorithm
+    (q_reward, sarsa_reward), (Q_q, Q_SARSA) = run_q_sarsa_comparison(
+        env=env,
+        total_runs=total_runs,
+        num_episodes=num_episodes,
+        epsilon=epsilon,
+        alpha=alpha,
+        gamma=gamma,
+    )
+
+    # plot result
+    plot_history(q_history=q_reward, sarsa_history=sarsa_reward)
