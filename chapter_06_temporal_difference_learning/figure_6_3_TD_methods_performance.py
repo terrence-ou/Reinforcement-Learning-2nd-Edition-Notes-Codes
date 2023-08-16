9fac7d42500d642e698fd4baa962975b4a54bc98
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-08-16 11:31:13 -0400

    figure 6.3 updated

---
+++ chapter_06_temporal_difference_learning/figure_6_3_TD_methods_performance.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+import numpy as np
+import matplotlib.pyplot as plt
+import gymnasium as gym
+
+
+def expectation(Q, state):
+    raise NotImplementedError
+
+
+def expected_SARSA():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    pass
